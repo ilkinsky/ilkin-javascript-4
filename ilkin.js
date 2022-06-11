fe623860f81b270e35df15864d36a016269619a8
ilkinsky <ilkinehmedzade35@gmail.com>
2022-06-11 11:16:34 +0400

    ilkin-javascript-4

---
+++ ilkin.js
@@ -0,0 +1,18 @@
+function upperCase() {
+    const x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+  }
+
+  function myFunction(x) {
+    x.style.background = "yellow";
+  }
+
+  function myFunction() {
+    document.getElementById("demo").innerHTML = "Sen sozu secdin";
+  }
+
+
+function myFunction() {
+  var x = document.getElementById("fname");
+  x.value = x.value.toUpperCase();
+}
